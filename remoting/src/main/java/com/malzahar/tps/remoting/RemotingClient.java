1dcaa63dde756dec97d80ac5f1a1ec6ef5de0c8c
momo <wang.yaxu@immomo.com>
2019-02-12 19:25:14 +0800

    初稿保存

---
+++ remoting/src/main/java/com/malzahar/tps/remoting/RemotingClient.java
@@ -0,0 +1,40 @@
+package com.malzahar.tps.remoting;
+
+
+import com.malzahar.tps.remoting.exception.RemotingConnectException;
+import com.malzahar.tps.remoting.exception.RemotingSendRequestException;
+import com.malzahar.tps.remoting.exception.RemotingTimeoutException;
+import com.malzahar.tps.remoting.exception.RemotingTooMuchRequestException;
+import com.malzahar.tps.remoting.netty.NettyRequestProcessor;
+import com.malzahar.tps.remoting.protocol.RemotingCommand;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+
+public interface RemotingClient extends RemotingService {
+
+    void updateNameServerAddressList(final List<String> addrs);
+
+    List<String> getNameServerAddressList();
+
+    RemotingCommand invokeSync(final String addr, final RemotingCommand request,
+                               final long timeoutMillis) throws InterruptedException, RemotingConnectException,
+            RemotingSendRequestException, RemotingTimeoutException;
+
+    void invokeAsync(final String addr, final RemotingCommand request, final long timeoutMillis,
+                     final InvokeCallback invokeCallback) throws InterruptedException, RemotingConnectException,
+            RemotingTooMuchRequestException, RemotingTimeoutException, RemotingSendRequestException;
+
+    void invokeOneway(final String addr, final RemotingCommand request, final long timeoutMillis)
+        throws InterruptedException, RemotingConnectException, RemotingTooMuchRequestException,
+        RemotingTimeoutException, RemotingSendRequestException;
+
+    void registerProcessor(final int requestCode, final NettyRequestProcessor processor,
+                           final ExecutorService executor);
+
+    void setCallbackExecutor(final ExecutorService callbackExecutor);
+
+    ExecutorService getCallbackExecutor();
+
+    boolean isChannelWritable(final String addr);
+}
